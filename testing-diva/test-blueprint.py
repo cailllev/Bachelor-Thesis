30e7d956ed3cb77ed5336604cdbeb5556fc6f184
cailllev <cailllev@students.zhaw.ch>
2021-04-14 08:16:22 -0400

    get block data

--- testing-diva/test-blueprint.py
+++ testing-diva/test-blueprint.py
@@ -1,6 +1,7 @@
 from setup.setup import download, setup, start_testnet, cleanup, API, EXPLORER
 
 import requests as req
+import json
 
 NODES = 7
 
@@ -10,9 +11,22 @@ def test():
 
 	is_ready = start_testnet()
 	if is_ready:
-		res = req.get(EXPLORER)
-		blockchain_data = res.text
-		print(blockchain_data)
+		blocks = (json.loads(req.get(f"{EXPLORER}/blocks").text))["blocks"]
+		pprint(blocks[-1])
+		input("wait...")
+
+		# instead of print(blocks) and input(), do some tests
+
+		# create new block?
+		# req.post("...diva-api/.../new block...", data="...")
+
+		# blocks[0] => first block
+		# blocks[-1] => last block
+
+		# iterate through all blocks
+		# for block in blocks:
+		#	 pprint(block)
+		#	 ...
 
 	else:
 		print("[!] TIMEOUT while trying to connect to DIVA.EXCHANGE explorer!")
