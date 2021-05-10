44b8a68cd485ac539407c642a1044e8405239f1f
cailllev <cailllev@students.zhaw.ch>
2021-05-10 07:12:18 -0400

    P3 strange results -> P3 overnight; add benchmark for remove with all nodes responding; rework P2 -> only do until first removed

--- testing-diva/P2.py
+++ testing-diva/P2.py
@@ -4,11 +4,12 @@ from setup.setup import download, setup, start_testnet, stop_testnet, delete
 from utils import *
 
 from pprint import pprint
+from time import time
 
 import os
 
 
-def test(peers, exhaustive):
+def test(peers):
 
 	try:
 		results = []
@@ -20,84 +21,50 @@ def test(peers, exhaustive):
 
 		if is_ready:
 		
-			# testing cycles, stopping:
-			# n2 ... peers
-			# n3 ... peers
-			# ...
-			# ...
-			# peers ... peers;
-			for i in range(5, peers):
+			print("[*] Stopping all peers ... (except API peer n1)")
+			stop_peers(2, peers)
+			print("[*] All peers stopped successfully.")
+
+			for i in range(1, peers):
 
 				print(f"\n****************************** start test round {i} **************************")
 
-				to_remove = peers # start with (trying to) remove last peer
-				removed_peers = []
-				timeout = 60
-
-				# stop all peers that are not in current cycle
-				print(f"[*] Stopping peers n{i+1} ... n{peers}.")
-				stop_peers(i+1, peers)
-
-				while True:
+				if i != 1: # API (n1) is always up
+					print(f"[*] Starting peer n{i}.")
+					start_peer(i)
 				
-					# try to remove peer (always the last first)
-					res = remove_peer(to_remove, timeout)
+				# 2f+1
+				if i <= peers / 2:
+					timeout = 60 # not expecting a remove
+				else:
+					timeout = 15 * 60  # expecting a remove
 
-					if res.status_code == 200:	
+				to_remove = peers
+				print(f"[#] Trying to remove peer n{to_remove} with timeout of {timeout} sec and {i} peers up.")
+				start_t = time()
+				res = remove_peer(to_remove, timeout)
+
+				if res.status_code == 200:	
 						
-						block = get_blocks()[0]
-						try:
-							signatures = get_signatures(block)
-							signers = get_signers(block)
-						except BaseException as e:
-							print("[!] Malformed block!\n")
-							pprint(block)
-							print()
-							signatures = 0
-							signers = []
-						results.append((i, f"n{to_remove}", len(signatures), signers))
+					block = get_blocks()[0]
+					duration = round(time() - start_t)
+					try:
+						signatures = get_signatures(block)
+						signers = get_signers(block)
+					except BaseException as e:
+						print("[!] Malformed block!\n")
+						pprint(block)
+						print()
+						signatures = 0
+						signers = []
+					results.append((i, f"n{to_remove}", duration, len(signatures), signers))
 
-						removed_peers.append(to_remove)						
-						to_remove -= 1
-						
-						still_up = min(i, to_remove+1)
-						print(f"[*] Peer n{to_remove+1} successfully removed with {still_up} peers up.")
+					print(f"[*] Peer n{to_remove} successfully removed after {duration} sec with {i} peers up => test complete!")
+					break
 
-						if not exhaustive:
-							print(f"[*] Non exhaustive mode => test round complete.")
-							break
-
-
-						if to_remove == 1:
-							print(f"[*] Removed all peers except n1 => test round complete.")
-							break
-
-					else:
-						print(f"[*] Could not remove peer n{to_remove} with {i} peers up out of {peers} peers => test round complete.")
-						results.append((i, "--", "--", "--"))
-						break
-
-
-				# start all peers (even the removes ones)
-				print(f"[*] Starting peers n{i+1} ... n{peers} again.")
-				start_peers(i+1, peers)
-
-				last_len_peers = len(get_peers())
-
-				# try to add all peers again
-				for peer in removed_peers:
-					res = add_peer(peer, timeout)
-					len_peers = len(get_peers())
-					
-					# check if really removed, otherwise clean delete and setup!
-					if len_peers != last_len_peers + 1 or res.status_code != 200:
-						print("[!] Could not add peers again, restart everything!")
-						print(f"[#] Status: {res.status_code}, Text: {res.text}")
-						stop_testnet()
-						delete()
-						setup(peers)
-						start_testnet(peers)
-						break
+				else:
+					print(f"[*] Could not remove peer n{to_remove} with {i} peers up out of {peers} peers.")
+					results.append((i, "--", timeout, "--", "--"))
 		
 		else:
 			print("\n[!] TIMEOUT while trying to connect to DIVA.EXCHANGE explorer!")
@@ -115,7 +82,7 @@ def test(peers, exhaustive):
 		delete()
 
 	try:
-		print(render_results(results, peers, ["peers up", "removed", "signs", "signers"], "P2"))
+		print(render_results(results, peers, ["peers up", "removed", "after", "signs", "signers"], "P2"))
 	
 	except BaseException as e:
 		print("\n[!] Unexpected Error!")
@@ -126,7 +93,6 @@ def test(peers, exhaustive):
 if __name__ == "__main__":
 	from sys import argv
 	optimalPeers = [9, 15, 21, 27, 33] # see 2f_3f_optimal.diff
-	exhaustive = False # after successful removal of peer ni, remove ni-1, ni-2, ..., n2
 	
 	if len(argv) > 1:
 
@@ -136,14 +102,14 @@ if __name__ == "__main__":
 				print("******************************************************************************")
 				print(f"[*] Starting test P2 with {peers} peers.")
 				print("******************************************************************************")
-				test(peers, exhaustive)
+				test(peers)
 
 		# test given peers count
 		else:
 			peers = int(argv[1])
-			test(peers, exhaustive)
+			test(peers)
 
 	# test default peers count
 	else:
 		peers = 9
-		test(peers, exhaustive=True)
+		test(peers)
--- testing-diva/P3.py
+++ testing-diva/P3.py
@@ -21,57 +21,58 @@ def test(peers):
 		is_ready = start_testnet(peers)
 
 		if is_ready:
-			print("[*] Stopping peers ...")
-			stop_peers(1, peers)
+			print("[*] Stopping all peers ... (except API peer n1)")
+			stop_peers(2, peers)
 			print("[*] All peers stopped successfully.")
-		
+				
+			# testing cycles
+			for i in range(1, peers+1):
+
+				print(f"\n****************************** start test round {i} **************************")
+
+				if i != 1: # API (n1) is always up
+					print(f"[*] Starting peer n{i}.")
+					start_peer(i)
+
+				# 2f+1
+				if i <= peers / 2:
+					timeout = 60 # not expecting a ping
+				else:
+					timeout = 15 * 60  # expecting a ping
+
+				waiting = 0
+				no_ping = False
+
+				# now wait for next ping
+				while len(blocks) <= last_len_blocks or not is_ping(blocks[0]):  # blocks[0] is the newest
+					blocks = get_blocks()
+					
+					print(f"\r[#] Wait for another ping, waited for {waiting} sec ...", end="")
+
+					if waiting >= timeout:
+						no_ping = True
+						break
+					
+					waiting += 1
+					sleep_till_whole_sec()
+
+
+				if no_ping:
+					print(f"\r[*] No other ping arrived after {waiting} sec with {i} peers up out of {peers} peers.")
+					results.append((i, waiting, "--", "--"))
+
+				else:
+					print(f"\r[*] Another ping arrived after {waiting} sec in block nr. {len(blocks)} with {i} peers up out of {peers} peers")
+					signatures = get_signatures(blocks[0])
+					signers = get_signers(blocks[0])
+					results.append((i, waiting, len(signatures), signers))
+					last_len_blocks = len(blocks)
+
 		else:
 			print("\n[!] TIMEOUT while trying to connect to DIVA.EXCHANGE explorer!")
 			print("[*] Test Failed!")
 			stop_testnet()
 			delete()
-				
-		# testing cycles
-		for i in range(1, peers+1):
-
-			print(f"\n****************************** start test round {i} **************************")
-
-			print(f"[#] Start peer n{i}.")
-			start_peer(i)
-
-			# 2f+1
-			if i <= peers / 2:
-				timeout = 60 # not expecting a ping
-			else:
-				timeout = 15 * 60  # expecting a ping
-
-			waiting = 0
-			no_ping = False
-
-			# now wait for next ping
-			while len(blocks) <= last_len_blocks or not is_ping(blocks[0]):  # blocks[0] is the newest
-				blocks = get_blocks()
-				
-				print(f"\r[#] Wait for another ping, waited for {waiting} sec ...", end="")
-
-				if waiting >= timeout:
-					no_ping = True
-					break
-				
-				waiting += 1
-				sleep_till_whole_sec()
-
-
-			if no_ping:
-				print(f"\r[*] No other ping arrived after {waiting} sec with {i} peers up out of {peers} peers.")
-				results.append((i, waiting, "--", "--"))
-
-			else:
-				print(f"\r[*] Another ping arrived after {waiting} sec in block nr. {len(blocks)} with {i} peers up out of {peers} peers")
-				signatures = get_signatures(blocks[0])
-				signers = get_signers(blocks[0])
-				results.append((i, waiting, len(signatures), signers))
-				last_len_blocks = len(blocks)
 
 	except KeyboardInterrupt:
 		print("\n[!] Aborting Test! Please wait!")
